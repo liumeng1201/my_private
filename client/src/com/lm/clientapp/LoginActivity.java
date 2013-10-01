5fbe74759793bb6e4a45334f109fe2d55daa4b94
liumeng <liumeng@liumeng-PC>
2013-10-01 10:21:07 +0800

    添加登陆功能，但还需要完善

--- client/src/com/lm/clientapp/LoginActivity.java
+++ client/src/com/lm/clientapp/LoginActivity.java
@@ -1,7 +1,14 @@
 package com.lm.clientapp;
 
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+
 import android.app.Activity;
-import android.app.AlertDialog;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -9,13 +16,17 @@ import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
+import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.lm.clientapp.utils.HttpTool;
 import com.lm.clientapp.utils.MyDialog;
 import com.lm.clientapp.utils.Utils;
 
@@ -35,15 +46,34 @@ public class LoginActivity extends Activity {
 	// 提示安装FlashPlayer的dialog
 	private MyDialog fpiDialog;
 
+	// 登陆提示对话框
+	private ProgressDialog loginDialog;
+	private static final int SHOW_LOGIN_DIALOG = 1000;
+	private static final int CANCEL_LOGIN_DIALOG = 1001;
+
 	private Context mContext;
+	private Handler mHandler = new Handler() {
+		@Override
+		public void handleMessage(Message msg) {
+			super.handleMessage(msg);
+			switch (msg.what) {
+			case SHOW_LOGIN_DIALOG:
+				loginDialog.show();
+				break;
+			case CANCEL_LOGIN_DIALOG:
+				loginDialog.dismiss();
+				break;
+			}
+		}
+	};
 
 	// 登录按钮点击事件监听
 	private OnClickListener loginClickListener = new OnClickListener() {
 		@Override
 		public void onClick(View v) {
-			String username = edtUsername.getText().toString();
-			String password = edtPassword.getText().toString();
-			String serverip = edtServerIP.getText().toString();
+			final String username = edtUsername.getText().toString();
+			final String password = edtPassword.getText().toString();
+			final String serverip = edtServerIP.getText().toString();
 			rmbUser = chbRememberUser.isChecked();
 			rmbIP = chbRememberIP.isChecked();
 
@@ -61,17 +91,47 @@ public class LoginActivity extends Activity {
 						});
 				dialog.show();
 			} else {
-				if (saveInfo(username, password, serverip, rmbUser, rmbIP)) {
-					Log.d(TAG, "save success");
-				}
+				// 用户登陆操作http请求地址
+				final String url = getResources().getString(R.string.loginUrl);
+				// 用户登录操作http传输数据
+				final List<NameValuePair> datas = new ArrayList<NameValuePair>();
+				// 添加数据
+				datas.add(new BasicNameValuePair("username", username));
+				datas.add(new BasicNameValuePair("password", password));
+				// 登陆提示进度条对话框
+				mHandler.sendEmptyMessage(SHOW_LOGIN_DIALOG);
+				// 在新线程中进行登陆操作
+				new Thread() {
+					@Override
+					public void run() {
+						super.run();
+						if (login(url, datas)) {
+							// 登陆成功,保存当前的用户输入以方便下次登陆
 
-				// 将服务器保存至全局变量中
-				ClientApp clientApp = (ClientApp) getApplication();
-				clientApp.setServerIP(serverip);
+							if (saveInfo(username, password, serverip, rmbUser,
+									rmbIP)) {
+								Log.d(TAG, "save success");
+							}
 
-				Intent intent = new Intent(mContext, MainActivity.class);
-				startActivity(intent);
-				LoginActivity.this.finish();
+							// 将服务器保存至全局变量中
+							ClientApp clientApp = (ClientApp) getApplication();
+							clientApp.setServerIP(serverip);
+
+							// 启动主界面Activity
+							Intent intent = new Intent(mContext,
+									MainActivity.class);
+							startActivity(intent);
+
+							// 取消登陆提示进度条对话框
+							mHandler.sendEmptyMessage(CANCEL_LOGIN_DIALOG);
+							LoginActivity.this.finish();
+						} else {
+							// 登陆失败,弹出对应的错误提示并取消登陆提示进度条对话框
+							mHandler.sendEmptyMessage(CANCEL_LOGIN_DIALOG);
+						}
+					}
+				}.start();
+
 			}
 		}
 	};
@@ -109,6 +169,48 @@ public class LoginActivity extends Activity {
 		});
 
 		btnLogin.setOnClickListener(loginClickListener);
+	}
+
+	// 登陆
+	private boolean login(String url, List<NameValuePair> datas) {
+		// 发送http请求,获得服务器返回数据
+		InputStream is = HttpTool.sendDataByPost(url, datas);
+		if (is != null) {
+			// 获取返回的数据
+			String result = HttpTool.convertStreamToString(is);
+			// 根据返回的数据获取用户ID
+			String userId = getUserIdFromResult(result);
+			// 验证用户身份
+			if ("null".equals(userId)) {
+				// 验证失败
+				mHandler.post(new Runnable() {
+					@Override
+					public void run() {
+						Toast.makeText(mContext, R.string.user_error,
+								Toast.LENGTH_SHORT).show();
+					}
+				});
+				return false;
+			} else {
+				// 验证成功
+				return true;
+			}
+		} else {
+			// 网络链接失败
+			mHandler.post(new Runnable() {
+				@Override
+				public void run() {
+					Toast.makeText(mContext, R.string.net_error,
+							Toast.LENGTH_SHORT).show();
+				}
+			});
+			return false;
+		}
+	}
+
+	// 从服务器返回的数据中获取用户ID
+	private String getUserIdFromResult(String result) {
+		return result;
 	}
 
 	// 载入之前保存的用户名、密码、服务器IP信息
