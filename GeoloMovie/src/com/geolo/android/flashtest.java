157ea437003c6e44af44ab13a12018a9685e0133
liumeng <liumeng@liumeng-PC>
2013-10-04 01:11:54 +0800

    flash player可以正常传值，webview加载url之后停顿一段时间再加载js脚本中的方法即可正常传值

--- GeoloMovie/src/com/geolo/android/flashtest.java
+++ GeoloMovie/src/com/geolo/android/flashtest.java
@@ -9,6 +9,7 @@ import android.view.View.OnClickListener;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings.PluginState;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.FrameLayout;
@@ -16,41 +17,54 @@ import android.widget.FrameLayout;
 @SuppressLint("NewApi")
 public class flashtest extends Activity {
 	Button btn = null;
+	FrameLayout fl = null;
 	Handler mHandler = new Handler();
 	WebView webview = null;
 	EditText ip;
+	Handler handler = null;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.test);
-
-		btn = (Button) findViewById(R.id.testbtnplay);
-
-		ip = (EditText) findViewById(R.id.testinput);
-
-		webview = (WebView) findViewById(R.id.webView02);
-
-		(webview.getSettings()).setJavaScriptEnabled(true);
-		(webview.getSettings()).setPluginState(PluginState.ON);
+		handler = new Handler();
+		setContentView(R.layout.testw2);
+		btn = (Button) findViewById(R.id.test2btn);
+		fl = (FrameLayout) findViewById(R.id.test2fl);
+		webview = new WebView(getApplicationContext());
+		webview.getSettings().setJavaScriptEnabled(true);
+		webview.getSettings().setPluginState(PluginState.ON);
 		webview.setWebChromeClient(new WebChromeClient());
-		webview.addJavascriptInterface(new CallJava(), "CallJava");
-//		webview.loadUrl("file:///android_asset/sample/Console.html");
-		webview.loadUrl("http://192.168.1.106:10000/flow/console/Console.html");
-
+		fl.addView(webview);
 		btn.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
-//				for (int i = 0; i < 2; i++) {
-//					webview.loadUrl("file:///android_asset/sample/tt.html");
-//					String message = ip.getText().toString();
-					webview.loadUrl("javascript:setValue('" + "1" + "')");
-//				}
+				// TODO Auto-generated method stub
+				webview.loadUrl("file:///android_asset/sample/tt.html");
+				new Thread() {
+					@Override
+					public void run() {
+						// TODO Auto-generated method stub
+						super.run();
+						try {
+							Thread.sleep(500);
+						} catch (InterruptedException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+						handler.post(new Runnable() {
+							@Override
+							public void run() {
+								// TODO Auto-generated method stub
+								webview.loadUrl("javascript:initFlash('" + "1" + "')");
+							}
+						});
+					}
+				}.start();
 			}
 		});
 
 	}
-
+	
 	private final class CallJava {
 		public void consoleFlashProgress(float progressSize) {
 		}
